83d0e8eb96ead523879143e97d57fdf6b1feb98b
pham anh nhat <phamanhnhat3210@gmail.com>
2021-08-25 16:10:15 +0700

    api: add method ProfileRepository

--- api/src/main/java/vn/com/sociallistening/manager/api/repository/mariadb/ProfileRepository.java
+++ api/src/main/java/vn/com/sociallistening/manager/api/repository/mariadb/ProfileRepository.java
@@ -2,9 +2,13 @@ package vn.com.sociallistening.manager.api.repository.mariadb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import vn.com.sociallistening.manager.entity.mariadb.Profile;
+import vn.com.sociallistening.manager.entity.mongodb.Profile;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
     Profile findByUsernameIgnoreCase(String username);
+
+    boolean existsByFacebookUrlContaining(String url);
+
+    boolean existsByFacebookId(String id);
 }
