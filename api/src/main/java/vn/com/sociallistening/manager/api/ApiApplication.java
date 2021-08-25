4c23260d5726d0ffe52ca62d8a19377d4ec8ac0d
pham anh nhat <phamanhnhat3210@gmail.com>
2021-08-25 16:17:04 +0700

    api: add config

--- api/src/main/java/vn/com/sociallistening/manager/api/ApiApplication.java
+++ api/src/main/java/vn/com/sociallistening/manager/api/ApiApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"vn.com.sociallistening.manager.entity.mariadb"})
 @EnableJpaRepositories(basePackages = {"vn.com.sociallistening.manager.api.repository"})
+@EnableMongoRepositories(basePackages = {"vn.com.sociallistening.manager.api.repository"})
 @EnableFeignClients
 public class ApiApplication {
     public static void main(String[] args) {
